6a3612db44722e483a627f10e10f89c66212b946
Nick Reisenauer <38516489+NickReisenauer@users.noreply.github.com>
2021-02-08 15:59:44 -0700

    Changes for new lesson

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,5 +1,42 @@
 import { GraphQLServer } from "graphql-yoga";
 
+// Demo user data
+const users = [
+  {
+    id: 1,
+    name: "Nick",
+    email: "nick@example.com",
+    age: 18,
+  },
+  { id: 2, name: "Andrew", email: "andrew@example.com", age: 27 },
+  { id: 3, name: "Mike", email: "mike@example.com" },
+];
+
+// Demo post data
+const posts = [
+  {
+    id: 1,
+    title: "AirPods",
+    body: "Apple AirPods Pro",
+    published: true,
+    author: 1,
+  },
+  {
+    id: 2,
+    title: "MacBook",
+    body: "Apple MacBook Pro",
+    published: true,
+    author: 2,
+  },
+  {
+    id: 3,
+    title: "Tesla Roadster",
+    body: "The 2022 Tesla Roadster: Coming Soon",
+    published: false,
+    author: 1,
+  },
+];
+
 // String
 // Boolean
 // Int
@@ -9,10 +46,9 @@ import { GraphQLServer } from "graphql-yoga";
 // Type Definitions
 const typeDefs = `
  type Query {
-   greeting(name: String position: String): String!
-   add(a: Float! b: Float!): Float!
+   users(query: String): [User!]!
    me: User!
-   post: Post!
+   posts(query: String): [Post!]!
  }
 
  type User {
@@ -27,16 +63,18 @@ const typeDefs = `
    title: String!
    body: String!
    published: Boolean!
+   author: User!
  }
 `;
 
 // Resolvers for our APIs
 const resolvers = {
   Query: {
-    greeting(parent, args, ctx, info) {
-      if (args.name && args.position)
-        return `Hello ${args.name}. Your position is an ${args.position}`;
-      return `Hello there!`;
+    users(parent, args, ctx, info) {
+      if (!args.query) return users;
+      return users.filter((user) =>
+        user.name.toLowerCase().includes(args.query.toLowerCase())
+      );
     },
 
     me() {
@@ -48,17 +86,23 @@ const resolvers = {
       };
     },
 
-    post() {
-      return {
-        id: "283482",
-        title: "Learning GraphQL",
-        body: "This is the body of my custom post type!",
-        published: true,
-      };
+    posts(parent, args, ctx, info) {
+      if (!args.query) return posts;
+      return posts.filter((post) => {
+        const isTitleMatch = post.title
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+        const isBodyMatch = post.body
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+        return isTitleMatch || isBodyMatch;
+      });
     },
+  },
 
-    add(parent, args, ctx, info) {
-      return args.a + args.b;
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => user.id === parent.author);
     },
   },
 };
